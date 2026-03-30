74531f94a29b69ffc482c7914a981ab81e16f808
Max Devs <ceo@maxdevs.com>
2026-03-29 22:39:38 -0300

    final: stabilize paths and cleanup diagnostic artifacts

--- donde_estoy.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "<h1>🔬 Suero de la Verdad Suito</h1>";
-echo "<b>Ruta física (Abstolute Path):</b> " . __FILE__ . "<br>";
-echo "<b>Directorios aquí (CWD):</b> " . getcwd() . "<br><br>";
-echo "<b>Archivos en esta carpeta:</b><br>";
-print_r(scandir('.'));
-?>
